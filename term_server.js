2b8a375dd486564268387fb28202fd7f143e4a74
Jerko Lannoo <jerko.lannoo@sgsintpaulus.eu>
2023-10-22 22:15:12 +0200

    small changes

--- term_server.js
+++ term_server.js
@@ -32,30 +32,100 @@ setInterval(() => {
   });
 }, 3600000);
 
-var users = {}
-
+setInterval(()=>{
+con.query("SELECT * FROM terminal_registration WHERE time>=2;", function(err,result){
+if(err) console.log(err)
+else{
+ for(let i=0; i<result.length;i++) io.to(result[i].socketId).emit('end-connection', 'Deze code is vervallen.');
+  con.query("DELETE FROM terminal_registration WHERE time>=2;", function(err){
+    if(err) console.log(err)
+    con.query("UPDATE terminal_registration SET time=time+1;", function(err){
+      if(err) console.log(err)
+      else console.log("success")
+    })
+  })
+}
+})
+}, 60000)
 const { Server } = require("socket.io");
 const io = new Server(server)
 io.on('connection', (socket) => {
-  console.log(socket);
-  console.log(socket[1]);
   socket.on("get-code", (bcode)=>{
     console.log("got socket request")
     let code=""
     let str="BCDFGHJKLMPQRTVWQYZ0123456789"
-    for(let i=0; i<4;i++) code+=str.charAt(Math.random()*str.length)
-    let id = socket.id
-    users[id] = code
-    console.log(users)
-    console.log("barcode: "+bcode)
-    con.query("INSERT INTO terminal_registration VALUES("+bcode+", "+JSON.stringify(code)+", "+JSON.stringify(id)+");", function(err,result){
-      if(err) console.log(err)
-      else io.emit("get-code",code)
-    })
+    return new Promise((resolve, reject)=>{
+      con.query("SELECT * FROM terminal_registration;", function(err, result){
+        console.log("in promise, result length: "+result.length)
+        if(err) reject(err)
+        else if(result.length){
+          let checkedCode=""
+          while(checkedCode===""){
+            console.log("finding code...")
+            for(let i=0; i<4;i++) code+=str.charAt(Math.random()*str.length)
+            for(let i=0;i<result.length;i++) {
+              if(result[i].code===code) return;
+              else if(i===result.length-1) {
+                checkedCode=code;
+                resolve(code)
+              }
+             } 
+          }
+        }
+        else {
+          for(let i=0; i<4;i++) code+=str.charAt(Math.random()*str.length)
+          resolve(code)
+        }
+      })
+    }).then(resolve=>{
+      console.log("resolved")
+      let id = socket.id
+      console.log("barcode: "+bcode)
+      con.query("INSERT INTO terminal_registration VALUES("+bcode+", "+JSON.stringify(code)+", "+JSON.stringify(id)+",0);", function(err,result){
+        if(err) console.log(err)
+        else io.emit("get-code",code)
+      })
+  }).catch(reject=>{
+    console.log(reject)
+  })
   })
 });
 app.post("/check-code", function(req,res){
   res.send({login: false})
+})
+app.post("/register/terminal/send-status", function(req,res){
+  console.log(req.body)
+  con.query("SELECT * FROM terminal_registration WHERE code="+JSON.stringify(req.body.code), function(err, result){
+    if(err) {
+      console.log(err)
+      res.send(500)
+    }
+    else if(result.length){
+      console.log("emitting to socket")
+      io.to(result[0].socketId).emit('register-status', 'true');
+    }
+    else {
+      console.log("socket not found")
+    }
+  })
+  res.sendStatus(200)
+})
+app.post("/register/terminal/send-scan", function(req,res){
+  console.log(req.body)
+  con.query("SELECT * FROM terminal_registration WHERE code="+JSON.stringify(req.body.code), function(err, result){
+    if(err) {
+      console.log(err)
+      res.send(500)
+    }
+    else if(result.length){
+      console.log("emitting to socket")
+      io.to(result[0].socketId).emit('scan-status', 'true');
+    }
+    else {
+      console.log("socket not found")
+    }
+  })
+  res.sendStatus(200)
 })
 app.get("/", function(req,res){
     res.sendFile(__dirname+"\\index.html")
